b6b8f5d2af3d162878e26a133ba4218793545cdc
Frank <frankmarsman@gmail.com>
2018-01-29 17:26:25 +0100

    ANother test, see if we get any log messages

--- skills/doubleq.js
+++ skills/doubleq.js
@@ -5,7 +5,9 @@ module.exports = function (controller) {
         bot.startConversation(message, function (err, convo) {
             convo.say('Welcome to the kebab store!');
             var response1;
+                              console.log("poitn1");
             convo.ask('What would you like on your kebab?', function (response, convo) {
+                      console.log("point2");
                 convo.say("Here you go, one kebab with " + response.text);
                       console.log("Done saying first thing");
                 response1.text = response.text;
