4f185c69f8da4a47559aff263ddeabb7d8b22a35
Frank <frankmarsman@gmail.com>
2018-01-29 17:33:51 +0100

    Move location where it might be defined

--- skills/doubleq.js
+++ skills/doubleq.js
@@ -1,10 +1,12 @@
+var response1;
+
 module.exports = function (controller) {
 
     controller.hears([/^kebab$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Welcome to the kebab store!');
-            var response1;
+            
                               console.log("poitn1");
             convo.ask('What would you like on your kebab?', function (response, convo) {
                       console.log("point2");
