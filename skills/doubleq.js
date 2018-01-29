2101bf5990e0d63bb2df572b628188522ca8203d
Frank <frankmarsman@gmail.com>
2018-01-29 17:48:42 +0100

    Move next call

--- skills/doubleq.js
+++ skills/doubleq.js
@@ -14,12 +14,12 @@ module.exports = function (controller) {
                       console.log("Done saying first thing");
                 response1 = response;
                       console.log("Done copying  repsonse");
-                convo.next();
+                
                       
             });
             console.log("Try to say sometoidfgnbhkj");
             convo.say("TEST " + response1.text);
-            
+            convo.next();
         });
 
     });
