65bc1a736740d318f9f8b25cc5a75d785370d684
Frank <frankmarsman@gmail.com>
2018-01-30 20:33:51 +0100

    Kebab is cleaned up

--- skills/doubleq.js
+++ skills/doubleq.js
@@ -1,4 +1,3 @@
-var response1;
 
 module.exports = function (controller) {
 
@@ -6,54 +5,13 @@ module.exports = function (controller) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Welcome to the kebab store!');
-            
-                              console.log("poitn1");
             convo.ask('What would you like on your kebab?', function (response, convo) {
-              console.log("point2");
-              convo.say("Here you go, one kebab with " + response.text);
-              console.log("Done saying first thing");
-              this.response1 = response;
-                      console.log("Done copying  repsonse");
-              console.log("Done copying  repsonse" + this.response1.text);
+                convo.say("Here's your kebab with " + response.text);
                 convo.next();
                       
             });
-                              
-                              convo.addQuestion("SOmething else?", function (response, convo) {
-                                                convo.say("You said " + response.text);
-                                                });
-                              
-
+                            
         });
                      
-        
-          
-                     
-                     // THIS BELOW DOESNT WORK!!!!
-//       bot.startConversation(message, function (err, convo) {
-//                             console.log("!! Try to say sometoidfgnbhkj");
-//                             convo.say("!! TEST " + this.response1.text);
-//                             convo.next();
-//
-//                             convo.say('Welcome to the kebab store! You ordered ' tis.response.text + "before");
-//
-//                             console.log("!! poitn1");
-//                             convo.ask('What would you like on your kebab?', function (response, convo) {
-//                                       console.log("!! point2");
-//                                       convo.say("!! Here you go, one kebab with " + response.text);
-//                                       console.log("!! Done saying first thing");
-//                                       this.response1 = response;
-//                                       console.log("!! Done copying  repsonse");
-//                                       console.log("!! Done copying  repsonse" + this.response1.text);
-//                                       convo.next();
-//
-//                                       });
-//
-//
-//                             });
-                     
-                     
-
-
     });
 };
